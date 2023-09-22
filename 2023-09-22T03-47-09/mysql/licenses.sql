6a243fc5f927a71c0cbf8a49ba77ea0c0ecebd49
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-09-22 10:46:47 +0700

    Upload backup file

---
+++ 2023-09-22T03-47-09/mysql/licenses.sql
@@ -0,0 +1,7 @@
+INSERT INTO licenses VALUES
+'boating', 'Boating License'
+'dmv', 'Code de la route'
+'drive', 'Drive License'
+'drive_bike', 'Bike License'
+'drive_truck', 'Truck License'
+'weapon', 'Weapon License';
