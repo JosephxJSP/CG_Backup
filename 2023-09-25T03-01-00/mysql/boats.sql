761fcad1ce7a9b33dd7694381f8b03a759b8a38d
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-09-25 10:00:21 +0700

    Upload backup file

---
+++ 2023-09-25T03-01-00/mysql/boats.sql
@@ -0,0 +1,7 @@
+INSERT INTO boats VALUES
+'dinghy3', 'dinghy3', 70000, 'boat'
+'jetmax', 'jetmax', 75000, 'boat'
+'marquis', 'marquis', 90000, 'boat'
+'seashark', 'seashark', 70000, 'boat'
+'Seashark2', 'Seashark2', 70000, 'boat'
+'seashark3', 'seashark3', 70000, 'boat';
