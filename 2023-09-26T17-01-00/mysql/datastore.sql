52d5c6395a86b05d62c69765aa50373575ab2ad1
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-09-27 00:01:17 +0700

    Upload backup file

---
+++ 2023-09-26T17-01-00/mysql/datastore.sql
@@ -0,0 +1,12 @@
+INSERT INTO datastore VALUES
+'property', 'Property', 0
+'society_ambulance', 'Ambulance', 1
+'society_chef', 'Chef', 1
+'society_council', 'Council', 1
+'society_mechanic', 'Mechanic', 1
+'society_police', 'Police', 1
+'user_ears', 'Ears', 0
+'user_glasses', 'Glasses', 0
+'user_helmet', 'Helmet', 0
+'user_mask', 'Mask', 0
+'vault', 'Vault', 0;
