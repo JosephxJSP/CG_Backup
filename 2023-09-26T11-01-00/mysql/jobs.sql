46f058260cb5b7d0a07fc62e5047a4c9c678e938
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-09-26 18:01:21 +0700

    Upload backup file

---
+++ 2023-09-26T11-01-00/mysql/jobs.sql
@@ -0,0 +1,13 @@
+INSERT INTO jobs VALUES
+'admin', 'Admin'
+'ambulance', 'Ems'
+'chef', 'Chef'
+'council', 'Council'
+'mechanic', 'Mechanic'
+'offambulance', 'Off-Duty'
+'offchef', 'Off-Duty'
+'offcouncil', 'Off-Duty'
+'offmechanic', 'Off-Duty'
+'offpolice', 'Off-Duty'
+'police', 'Police'
+'unemployed', 'Unknow';
