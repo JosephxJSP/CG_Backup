d55c978501ac5cf9bbacd608ba8010956aeb3577
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-09-29 10:01:07 +0700

    Upload backup file

---
+++ 2023-09-29T03-01-00/mysql/addon_account.sql
@@ -0,0 +1,9 @@
+INSERT INTO addon_account VALUES
+'property_black_money', 'Money Sale Property', 0
+'society_ambulance', 'Ambulance', 1
+'society_chef', 'Chef', 1
+'society_council', 'Council', 1
+'society_mechanic', 'Mechanic', 1
+'society_police', 'Police', 1
+'society_police_black_money', 'Police Black Money', 1
+'vault_black_money', 'Money Vault', 0;
