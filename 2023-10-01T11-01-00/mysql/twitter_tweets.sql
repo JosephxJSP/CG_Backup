418273fd156ad16661cbb640994aa778d26ee8da
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-10-01 18:02:05 +0700

    Upload backup file

---
+++ 2023-10-01T11-01-00/mysql/twitter_tweets.sql
@@ -0,0 +1,2 @@
+INSERT INTO twitter_tweets VALUES
+3176123, 9859, 'steam:110000135e7e110', '', 'https://media.discordapp.net/attachments/1138905855174135951/1157994480465612941/screenshot.jpg?ex=651aa1e6&is=65195066&hm=db35da3d71addfb648ae037f7a02e00bb457693d1917a23f854dfa3cc4edbc32&', '2023-10-01 17:56:40.000', 0;
