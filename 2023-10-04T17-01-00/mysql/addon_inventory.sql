e0eead796089c66f6fe27be58b27c59214ee1824
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-10-05 00:01:10 +0700

    Upload backup file

---
+++ 2023-10-04T17-01-00/mysql/addon_inventory.sql
@@ -0,0 +1,10 @@
+INSERT INTO addon_inventory VALUES
+'property', 'Property', 0
+'society_ambulance', 'Ambulance', 1
+'society_cardealer', 'Concesionnaire', 1
+'society_chef', 'Chef', 1
+'society_council', 'Council', 1
+'society_mechanic', 'Mechanic', 1
+'society_police', 'Police', 1
+'society_police2', 'Police', 1
+'vault', 'Vault', 0;
