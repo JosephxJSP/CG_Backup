2725f048858b338da9d1d20993f49e4e84b4c00a
JosephxJSP <111652019+JosephxJSP@users.noreply.github.com>
2023-10-24 00:01:58 +0700

    Upload backup file

---
+++ 2023-10-23T17-01-00/mysql/vehicle_categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO vehicle_categories VALUES
+'KG', 'เธฃเธ–เธ—เธณเธเธฒเธ'
+'Mc', 'Mc (เธฎเธฒเน€เธฅเธขเน)'
+'Muscle', 'Muscle'
+'Suv', 'Suv'
+'bike', 'bike'
+'hermes', 'Hermes'
+'motor', 'Motorcycle'
+'sport', 'Sport'
+'sport_classic', 'Sport Classic'
+'super', 'Super';
